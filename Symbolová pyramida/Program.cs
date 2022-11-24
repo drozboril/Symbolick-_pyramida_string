295a5f65b8d3e8c897699af4f18fd7829ae5a2c1
drozboril <rozborildavid77@gmail.com>
2022-11-24 21:11:50 +0100

    Uživatel zadá výšku pyramidy a symbol který program využije k jejímu vypsání. Ačkoliv se může zdát že tenhle program napodobuje svého předchůce tak ve skutečnosti byl vytvořen za účelem využití alternativních možností, které se vyskytly právě při tvorbě původního programu. Skvělým příkladem je např. druh vstupu, předchozí program používá "char" zatímco tenhle program pracuje se "string".

---
+++ Symbolová pyramida/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Symbolová_pyramida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte max. 10 řádků");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadejte symbol");
+            string symbol = Console.ReadLine();
+            int pocet = 1;
+
+            if (num <= 1 || num > 10 || symbol == string.Empty || symbol == " " || symbol.Length > 1)
+            {
+                Console.WriteLine("Chyba zadání\nProgram ukončete stisknutím klávesy Enter");
+                Console.ReadLine();
+            }
+            else
+            {
+                num = num * 2;
+                for (int radky = num; radky >= 1; radky--)
+                {
+                    for (int mezery = radky - 1; mezery >= 1; mezery--)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int Sym = 1; Sym <= pocet; Sym++)
+                    {
+                        if (pocet % 2 != 0)
+                        {
+                            Console.Write("{0}", symbol);
+                            Console.Write(" ");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    pocet++;
+                    Console.WriteLine();
+                }
+                Console.WriteLine("\nProgram ukončete stisknutím klávesy Enter");
+                Console.ReadLine();
+            }
+        }
+    }
+}
